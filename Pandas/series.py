25722cee61b45820817e75f82923434f5d792803
ABDUL MOIZ <babamoiz289@gmail.com>
2025-03-16 04:00:31 +0500

    Pandas Series Topic

---
+++ Pandas/series.py
@@ -0,0 +1,5 @@
+# This is a series output as a table index show like a serial number or first column
+import pandas as pd
+
+s = pd.Series([1, 2, 3, 4, 5], index=["a", "b", "c", "d", "e"])
+print(s)
